d0884c51f235eb204d6f7912789e655688fbaf0f
BwSTraps <72046879+BwSTraps@users.noreply.github.com>
2020-12-04 13:10:27 -0600

    Update profundidad.py

--- Unidad 3/profundidad/profundidad.py
+++ Unidad 3/profundidad/profundidad.py
@@ -1,24 +1,37 @@
 import json
-
-def profundidad():
+from sys import exit
+def profundidad(x,y):
     with open ("recorrido.json", "r") as read_file:
         data = json.load(read_file)
         grafo = data['nodos']
         
     siguiente=[]
+    x=False
 
     while len(grafo)!=0:
         
         del siguiente[:]
         raiz=grafo[0][0]
-        print(raiz)
+        
+        
+        print(raiz," s1")
+        
+        if raiz == y:
+            exit()
+        
+
+        
         contador=0;
         for nodo in  grafo:
             if raiz == nodo[0]:
                 raiz = nodo[1]
-                print(raiz)
+                print(raiz, " cf1")
                 siguiente.append(contador)
+                if raiz == y:
+                    exit()
+
             contador=contador+1;
+
         
         siguiente = sorted(siguiente, reverse=True)
 
@@ -29,7 +42,12 @@ def profundidad():
 
         raiz=grafo[(len(grafo)-1)][1]
         
-        print(raiz)
+        print(raiz,"  s2")
+        
+        if raiz==y:
+            exit()
+        
+
         
         grafo2 = sorted(grafo, reverse=True)
         
@@ -37,5 +55,6 @@ def profundidad():
             if raiz == nodo[1]:
                 raiz = nodo[0]
                 grafo.pop(grafo.index(nodo))
+        
                 
-profundidad()
+profundidad("10","5")
