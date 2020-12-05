2aaf257be023cd08f5c055840829d609656ecd28
BwSTraps <72046879+BwSTraps@users.noreply.github.com>
2020-12-04 19:22:53 -0600

    Update reinas.py

--- Unidad 3/reinas/reinas.py
+++ Unidad 3/reinas/reinas.py
@@ -5,128 +5,125 @@ Created on Fri Nov 13 10:44:15 2020
 @author: Cristian
 """
 
-tablero=[
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0]
-    ]
+reina = 4
+posicion =[]
+llave = 0
+tabldr =[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+xls = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
-print(tablero)
 
-def reinas(tablero):
-    listaindices=[]
-    for n, fila in enumerate(tablero):
-        print("------------")
+def reinacont(reina, tabldr, llave, xls):
+    tbldr2 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    m = 0
+    print("# de reinas: "+str(reina))
+    if reina == 1:
+        return fin(tabldr)
+    else: 
+        for i in range(len(tabldr)):
+            for j in range(len(tabldr[i])):
+                if m == 0:
+                  if xls[i][j] != 3:
+                        if(tabldr[i][j] == 0):
+                            if reina == 4:
+                                xls[i][j] = 3
+                            tabldr[i][j] = 1
+                            m = m + 1
+                            posicion.append([i,j])
+        tabldr = arrg(tabldr,posicion)
+        posicion.pop(0)
+        for i in tabldr:
+            if 0 in i:
+               llave = 0
+            else:
+               llave = 1
+        if llave == 1:
+            print(llave)
+            for i in tbldr2:
+                print(i)
+            reina=4
+            llave=0
+            return reinacont(reina,tbldr2,llave,xls)
+        if llave == 0:
+            print(llave)
+            reina=reina-1
+            return reinacont(reina,tabldr,llave,xls)
+        
         
 
-        for x,celda in enumerate(fila):
-            if tablero[n][x] == 0:
-                tablero[n][x] = 1
-                #dar valores de 2
-                print()
-                for t in tablero:
-                    print (t)
-                
-                for column, celdacel in enumerate(fila):
-                    if tablero[n][column] == 0:
-                       tablero[n][column]=2 
-                for ar, arrow in enumerate(tablero):
-                    if tablero[ar][x]==0:
-                       tablero[ar][x]=2 
-                filawhile=n
-                columnawhile=x
-                print()
-                for t in tablero:
-                    print (t)
-                ####diagonal inferior derecha
-                for i in range(0, 3, 1):
-                    if columnawhile==3 or filawhile==3 or filawhile<0 or columnawhile<0:
-                        break
+def arrg(tabldr,posicion):
+    print("recorrido")
+    a = posicion[0][0]
+    b = posicion[0][1]
+    j = range(len(tabldr))
+    d = b
+    c = a
+    print(posicion)
+    
+    for i in range(len(tabldr)):
+        for j in range(len(tabldr[i])):
+            tabldr[a][j] = 2
+            tabldr[i][b] = 2
+    printf(tabldr)
+    for t in range(len(tabldr)):
+        d = d - 1
+        c = c - 1
+        if (d >= 0)and(c >=0):
+            print("- -")
+            tabldr[c][d] = 2
+    d = b
+    c = a
+    printf(tabldr)
+    for t in range(len(tabldr)):
+        c = c + 1
+        d = d + 1
+        if (c <= j)and(d <= j):
+            print("+ +")
+            tabldr[c][d] = 2
 
-                    filawhile=filawhile+1
-                    columnawhile=columnawhile+1
-                    j=filawhile,columnawhile
-                    listaindices.append(j)
-                print()
-                for t in tablero:
-                    print (t)
-                
-                print(listaindices)
-                for li in listaindices:
-                    tablero[li[0]][li[1]]=2
-                print(listaindices)
-                listaindices=[]
-                print()
-                for t in tablero:
-                    print (t)
-                fw=n
-                cw=x
-                
-                ####diagonal inferior derecha
-                for i in range(0, 3, 1):
-                    if cw<0 or fw<0 or cw==3 or fw==3:
-                        break
-                    fw=fw+1
-                    cw=cw-1
-                    m=fw,cw
-                    listaindices.append(m)
-                print(listaindices,"sentido contrario")
-                for li in listaindices:
-                    tablero[li[0]][li[1]]=2
-                print(listaindices,"guitarra")
-                listaindices=[]
-                
-                print()
-                for t in tablero:
-                    print (t)
-                
-                ####diagonal inferior derecha----------------------
-                fw=n
-                cw=x
-                for i in range(0, 3, 1):
-                    if cw==0 or fw==0 or cw==3 or fw==3:
-                        break
-                    fw=fw-1
-                    cw=cw+1
-                    m=fw,cw
-                    listaindices.append(m)
-                print(listaindices,"sentido contrario")
-                for li in listaindices:
-                    tablero[li[0]][li[1]]=2
-                listaindices=[]
-                print()
-                for t in tablero:
-                    print (t)
-                                
-                ####diagonal inferior derecha----------------------
-                fw=n
-                cw=x
-                for i in range(0, 3, 1):
-                    if cw==0 or fw==0 or cw==3 or fw==3:
-                        break
-                    fw=fw-1
-                    cw=cw-1
-                    m=fw,cw
-                    listaindices.append(m)
-                print(listaindices,"sentido contrario")
-                for li in listaindices:
-                    tablero[li[0]][li[1]]=2
-                listaindices=[]
-                
-                print()
-                for t in tablero:
-                    print (t)
-    
-                    
-    print(filawhile,",-----------",columnawhile)
-    terminar=0
-    for t in tablero:
-        for r in t:
-         if r==1:
-             terminar=terminar+1
-    print(terminar)
-    for t in tablero:
-            print (t)
-    
-reinas(tablero)
+    d = b
+    c = a
+    printf(tabldr)
+    for i in range(len(tabldr)):
+        d = d+1
+        c = c -1
+        if (d < j)and(c >=0):
+            print("+ -")
+            tabldr[c][d] = 2
+    d = b
+    c = a
+    printf(tabldr)
+    for t in range(len(tabldr)):
+        d = d-1
+        c = c + 1
+        if (d >= 0)and(c <j):
+            print("- +")
+            tabldr[c][d] = 2
+    tabldr[a][b]= 1
+    printf(tabldr)
+    print("")
+    print("posicionado con éxito")
+    return tabldr
+
+
+   
+def fin(tabldr):
+    for i in range(len(tabldr)):
+        for j in range(len(tabldr[i])):
+            if (tabldr[i][j]) == 0:
+                tabldr[i][j] = 1
+    print("tablero finale")
+    print("")
+    print("************")
+    for i in tabldr:
+        print(i)
+        
+        
+
+def printf(tabldr):
+   print("")
+   print("------------------------")
+   for i in tabldr:
+       print(i)
+       
+
+reinacont(reina,tabldr,llave,xls)
